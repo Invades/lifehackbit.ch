7b9b1999ae3da40ee5c6cee09bd52a4d250bb289
Invades <invades@tutanota.com>
2025-09-14 18:30:25 +0300

    Fix background and input component styling

--- src/lib/components/input.svelte
+++ src/lib/components/input.svelte
@@ -15,5 +15,5 @@
     {autocomplete}
     bind:value
     on:input={onInput}
-    class="text-sm bg-[rgba(255,255,255,0.05)] border border-white border-opacity-10 text-[rgba(255,255,255,0.8)] rounded backdrop-blur text-[0.9em] p-2 opacity-70 placeholder:text-[rgba(255,255,255,0.5)] transition-all duration-200 focus:opacity-100 focus:border-opacity-20 {className}"
+    class="w-full min-w-0 text-sm bg-[rgba(255,255,255,0.05)] border border-white border-opacity-10 text-[rgba(255,255,255,0.8)] rounded backdrop-blur text-[0.9em] p-1.5 sm:p-2 opacity-70 placeholder:text-[rgba(255,255,255,0.5)] transition-all duration-200 focus:opacity-100 focus:border-opacity-20 {className}"
 />
